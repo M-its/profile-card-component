893684119de55f1756c6dbdedbd6a9ac08afa265
Mitsrael Souza <mitarael9@gmail.com>
2023-09-18 23:51:41 -0300

    finished background

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,20 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
-            backgroundImage: {
-                "pattern-top": "url('/images/bg-pattern-top.svg')",
-                "pattern-bottom": "url('/images/bg-pattern-bottom.svg')",
+            fontFamily: {
+                // redhd: ["Red Hat Display", "sans-serif"],
+                "kumbh-sans": ["Kumbh Sans", "sans-serif"],
+            },
+
+            colors: {
+                // Primary
+                "dark-cyan": "hsl(var(--dark-cyan))",
+                "very-dark-desaturated-blue":
+                    "hsl(var(--very-dark-desaturated-blue))",
+                "dark-grayish-blue": "hsl(var(--dark-grayish-blue))",
+
+                // Neutral
+                "dark-gray": "hsl(var(--dark-gray))",
             },
         },
     },
